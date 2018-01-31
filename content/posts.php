17286a51337dedab7a7f611dcea42d089c59c293
MickeyNS <martons@gmail.com>
2018-01-31 19:41:47 +0100

    Zadatak 2

---
+++ content/posts.php
@@ -0,0 +1,7 @@
+<?php include 'template/header.php'; ?>
+
+<?php include 'content/single-post1.php'; ?>
+<?php include 'content/single-post2.php'; ?>
+<?php include 'content/single-post3.php'; ?>
+
+<?php include 'template/footer.php'; ?>
